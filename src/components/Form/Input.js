535cbb1f3bc8ad5684157d42dba38b345ef4fea5
mralpha125 <sulimansayed003@gmail.com>
2023-06-25 15:38:03 +0500

    form validations in apply form

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -3,12 +3,13 @@ import { Form } from 'react-bootstrap'
 import PropTypes from "prop-types";
 import "./../style/Form/input.scss";
 
-export default function Input({ label, name, type, placeholder, value, handleChange, controlId, error }) {
+export default function Input({ label, name, type, placeholder, value, handleChange, controlId, error, touched, required }) {
+    console.log('hey')
     return (
         <Form.Group className='mb-3' controlId={controlId}>
-            {label ?? <Form.Label className="input-label mb-0">{label}</Form.Label>}
-            <Form.Control onChange={handleChange} className="form-input shadow-none" isInvalid={error} type={type} placeholder={placeholder} name={name} />
-            <Form.Control.Feedback type='invalid'>{error}</Form.Control.Feedback>
+            {label && <Form.Label className="input-label mb-0">{label}{required && <span className='text-danger'>*</span>}</Form.Label>}
+            <Form.Control required={required} onChange={handleChange} className="form-input shadow-none" isValid={touched} isInvalid={error} type={type} placeholder={placeholder} name={name} />
+            {error && <Form.Control.Feedback type='invalid'>{error}</Form.Control.Feedback>}
         </Form.Group>
     );
 }
@@ -19,6 +20,8 @@ Input.propTypes = {
     placeholder: PropTypes.string.isRequired,
     value: PropTypes.string,
     controlId: PropTypes.string,
+    required: PropTypes.bool,
+    error: PropTypes.string
 };
 Input.defaultProps = {
     placeholder: PropTypes.string.isRequired
--- src/pages/Apply-Form.jsx
+++ src/pages/Apply-Form.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useFormik } from 'formik';
 import { ToastContainer } from "react-toastify";
-import { toast } from "react-toastify";
 import { Button, Col, Container, Form, Row, Stack } from "react-bootstrap";
 import Card from "components/Card";
 import Input from "components/Form/Input";
@@ -10,50 +9,64 @@ const ApplyForm = () => {
     const [validated, setValidated] = useState(false);
     // const navigate = useNavigate();
     const schema = yup.object().shape({
-        fname: yup.string().required()
+        fName: yup.string().required(),
+        lName: yup.string().required(),
+        pNumber: yup.string().required(),
+        accNum: yup.string().required(),
+        ssn: yup.string().required(),
+        email: yup.string().email('Invalid Email').required('Required'),
+        cEmail: yup.string().email('Invalid Email').oneOf([yup.ref('email'), null], 'Email doesn\'t match').required('Required')
     });
     const formik = useFormik({
         validationSchema: schema,
         initialValues: {
-            email: 'sulimansayed003@gmial.com',
+            email: '',
             password: ''
         },
-        onSubmit: ({ email, password }) => {
+        onSubmit: (values) => {
             setValidated(false);
+            console.log('submitted Values', values);
         }
     });
-    console.log(formik.touched)
+    const { errors, touched, handleSubmit, handleChange } = formik;
     return (
         <Container>
             <ToastContainer />
             <Row className="min-vh-100 align-items-center justify-content-center my-auto">
-                <Col md={10} lg={8} xl={6}>
-                    <Form noValidate onSubmit={formik.handleSubmit} validated={validated}>
+                <Col md={10} lg={8} xl={7} xxl={6}>
+                    <Form noValidate onSubmit={handleSubmit} validated={validated}>
                         <Card title='Application Form'>
                             <Row className="mt-5">
                                 <Col md={6}>
-                                    <Input label='First Name*' controlId='controlId-1' name='fname' type='text' placeholder='Mr.' handleChange={formik.handleChange} error={formik.errors.fname} />
+                                    <Input label='First Name' required controlId='controlId-fName' name='fName' type='text' placeholder='Mr.'
+                                        handleChange={handleChange} touched={touched.fName && !errors.fName} error={errors.fName} />
                                 </Col>
                                 <Col md={6}>
-                                    <Input label='Middle Name' name='mname' type='text' placeholder='Middle Name' handleChange={formik.handleChange} />
+                                    <Input label='Middle Name' name='mName' type='text' placeholder='Middle Name' handleChange={handleChange} />
                                 </Col>
                                 <Col md={6}>
-                                    <Input label='Last Name*' name='lname' type='text' placeholder='Last Name' handleChange={formik.handleChange} />
+                                    <Input label='Last Name' required controlId='controlId-lName' name='lName' type='text' placeholder='Last Name'
+                                        handleChange={handleChange} touched={touched.lName && !errors.lName} error={errors.lName} />
                                 </Col>
                                 <Col md={6}>
-                                    <Input label='Phone Number*' name='pnumber' type='number' placeholder='Phone Number' handleChange={formik.handleChange} />
+                                    <Input label='Phone Number' required name='pNumber' controlId='controlId-pNumber' type='number' placeholder='Phone Number'
+                                        handleChange={handleChange} touched={touched.pNumber && !errors.pNumber} error={errors.pNumber} />
+                                </Col>
+                                <Col md={8}>
+                                    <Input label='Account Number' required name='accNum' controlId='controlId-accNum' type='number' placeholder='Account Number'
+                                        handleChange={handleChange} touched={touched.accNum && !errors.accNum} error={errors.accNum} />
+                                </Col>
+                                <Col md={4}>
+                                    <Input label='Last 4 digits of SSN' required controlId='controlId-ssn' name='ssn' type='number' placeholder='SSN'
+                                        handleChange={handleChange} touched={touched.ssn && !errors.ssn} error={errors.ssn} />
                                 </Col>
                                 <Col md={6}>
-                                    <Input label='Last 4 digits of SSN*' name='mname' type='number' placeholder='SSN' handleChange={formik.handleChange} />
+                                    <Input label='Email' required controlId='controlId-email' name='email' type='email' placeholder='Email'
+                                        handleChange={handleChange} touched={touched.email && !errors.email} error={errors.email} />
                                 </Col>
                                 <Col md={6}>
-                                    <Input label='Account Number*' name='mname' type='number' placeholder='Account Number' handleChange={formik.handleChange} />
-                                </Col>
-                                <Col md={6}>
-                                    <Input label='Email*' name='mname' type='email' placeholder='Email' handleChange={formik.handleChange} />
-                                </Col>
-                                <Col md={6}>
-                                    <Input label='Confirm Email*' name='mname' type='email' placeholder='Confirm Email' handleChange={formik.handleChange} />
+                                    <Input label='Confirm Email' required controlId='controlId-cEmail' name='cEmail' type='email' placeholder='Confirm Email'
+                                        handleChange={handleChange} touched={touched.cEmail && !errors.cEmail} error={errors.cEmail} />
                                 </Col>
                                 <Col md={8} className="mt-4">
                                     <Stack>

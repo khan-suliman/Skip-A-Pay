249cd463aec6773cfd2d68a45e1ccd1cfdaa0f49
Suliman Khan <sulimank418@gmail.com>
2023-08-31 11:25:26 +0500

    Sever URI correct

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const config = {
   siteName: "Skip A Pay",
   logo: "",
   hideSidebar: 992,
-  server: "https://skipapay-api.zeabur.app",
+  server: "https://skipapay-api.zeabur.app/",
   // server: "https://skipapay-api.vercel.app/",
   // server: "http://localhost:3001/",
 };
